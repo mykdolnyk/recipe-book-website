2254af37df7de2943c2d13a8cca187ba1555ef01
mykdolnyk <mdolnyk@gmail.com>
2025-10-11 17:05:35 +0300

    Connected essential modules for Flask Connected the following modules:     - Flask-SQLAlchemy     - Flask-Migrate     - Flask-Login

--- app/app_factory.py
+++ app/app_factory.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
 
 
 def create_app(config_object):
     app = Flask(__name__)
     app.config.from_object(config_object)
     
+    db.init_app(app=app)
+    migrate.init_app(app=app, db=db)
+    login_manager.init_app(app=app)
+
     return app
--- app/run.py
+++ app/run.py
@@ -1,7 +1,7 @@
 from app_factory import create_app
+import config
 
-
-app = create_app(config_object=object)
+app = create_app(config_object=config)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
