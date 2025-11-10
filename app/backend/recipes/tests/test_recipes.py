50246eebacd08646f508431f4dd970ebdd4337c6
mykdolnyk <mdolnyk@gmail.com>
2025-11-10 15:25:50 +0200

    Added tests for Recipe operations

---
+++ app/backend/recipes/tests/test_recipes.py
@@ -0,0 +1,133 @@
+from flask.testing import FlaskClient
+from flask_login import current_user, login_user, logout_user
+
+from backend.recipes.models import Recipe
+
+
+def test_create_recipe(client: FlaskClient, logged_in_user):
+    name = "Tasty meal"
+    response = client.post('/api/recipes', json={
+        "name": name,
+        "calories": "4",
+        "cooking_time": "1337",
+        "ingredients": "Water",
+        "text": "A very long recipe here",
+        "period_type_id": 1,
+    })
+    assert response.status_code == 200
+    assert response.get_json()['name'] == name
+    assert response.get_json()['author']['id'] == current_user.id
+
+    # logged-out request
+    logout_user()
+    response = client.post('/api/recipes', json={
+        "name": name,
+        "calories": "4",
+        "cooking_time": "1337",
+        "ingredients": "Water",
+        "text": "A very long recipe here",
+        "period_type_id": 1,
+    })
+    assert response.status_code == 401
+
+
+def test_edit_recipe(client: FlaskClient, logged_in_user, test_superuser, test_recipe):
+    new_name = "Tasty meal"
+    assert test_recipe.name != new_name
+
+    test_recipe.author_id = logged_in_user.id
+    Recipe.query.session.commit()
+    response = client.put(f'/api/recipes/{test_recipe.id}', json={
+        "name": new_name,
+    })
+    assert response.status_code == 200
+    assert response.get_json()['name'] == new_name
+
+    # logged-out request
+    logout_user()
+    new_name = "Forbidden meal"
+    response = client.put(f'/api/recipes/{test_recipe.id}', json={
+        "name": new_name,
+    })
+    assert response.status_code == 403
+
+    # superuser request
+    login_user(test_superuser)
+    new_name = "Super meal"
+    response = client.put(f'/api/recipes/{test_recipe.id}', json={
+        "name": new_name,
+    })
+    assert response.status_code == 200
+    assert response.get_json()['name'] == new_name
+    assert response.get_json()['author']['id'] == logged_in_user.id
+
+
+def test_get_recipe(client: FlaskClient, logged_in_user):
+    # create a recipe
+    name = 'Getting a Recipe'
+    response = client.post('/api/recipes', json={
+        "name": name,
+        "calories": "4",
+        "cooking_time": "1337",
+        "ingredients": "Water",
+        "text": "A very long recipe here",
+        "period_type_id": 1,
+    })
+
+    recipe_id = response.get_json()['id']
+
+    # get a recipe
+    response = client.get(f'/api/recipes/{recipe_id}')
+    assert response.status_code == 200
+    assert response.get_json()['name'] == name
+
+    # get a recipe with `is_visible` set to `False`
+    recipe: Recipe = Recipe.query.filter_by(id=recipe_id).first()
+    recipe.is_visible = False
+    Recipe.query.session.commit()
+
+    response = client.get(f'/api/recipes/{recipe_id}')
+    assert response.status_code == 404
+    # TODO: once recipe publication is implemented, test it
+
+
+def test_get_recipe_list(client: FlaskClient, test_set_of_recipes):
+    response = client.get('/api/recipes')
+    assert response.status_code == 200
+    assert response.get_json()["total"] == len(test_set_of_recipes['visible'])
+
+    # Checking pagination
+    assert response.get_json()['per_page'] == 5
+    assert response.get_json()['pages'] == 2
+    # Exceeding max per page
+    response = client.get('/api/recipes?per-page=30')
+    assert response.status_code == 200
+    assert response.get_json()['per_page'] == 25
+    assert response.get_json()['pages'] == 1
+    # Incorrect params
+    response = client.get('/api/recipes?per-page=hello')
+    assert response.status_code == 400
+
+
+def test_delete_recipe(client: FlaskClient, logged_in_user, test_superuser, test_recipe):
+    # wrong user:
+    response = client.delete(f'/api/recipes/{test_recipe.id}')
+    assert response.status_code == 403
+
+    # correct user:
+    test_recipe.author_id = logged_in_user.id
+    Recipe.query.session.commit()
+    response = client.delete(f'/api/recipes/{test_recipe.id}')
+    assert response.status_code == 204
+    response = client.get(f'/api/recipes/{test_recipe.id}')
+    assert response.status_code == 404
+
+    # un-delete the recipe and delete as a superuser
+    test_recipe.is_visible = True
+    Recipe.query.session.commit()
+    logout_user()
+    login_user(test_superuser)
+    response = client.delete(f'/api/recipes/{test_recipe.id}')
+    assert response.status_code == 204
+    response = client.get(f'/api/recipes/{test_recipe.id}')
+    assert response.status_code == 404
--- app/conftest.py
+++ app/conftest.py
@@ -1,4 +1,7 @@
+import flask_login
 import pytest
+from backend.recipes.models import Recipe, RecipeTag
+from backend.recipes.schemas import RecipeCreate, RecipeTagCreate
 from backend.users.models import User
 from backend.users.schemas import UserCreate
 from backend.users.helpers import create_user_instance
@@ -141,3 +144,80 @@ def create_users() -> dict[str, list[User]]:
     db.session.commit()
 
     return users
+
+
+@pytest.fixture
+def logged_in_user(app):
+    schema = UserCreate(
+        name='Logged In User',
+        email='testing@test.com',
+        password=TEST_PASSWORD,
+        password_confirm=TEST_PASSWORD
+    )
+    user = create_user_instance(user_schema=schema)
+    
+    with app.test_request_context():
+        flask_login.login_user(user)
+        yield user
+        
+
+@pytest.fixture
+def test_recipe(app):
+    recipe = Recipe(
+            name="Test Recipe",
+            calories="4",
+            cooking_time="1337",
+            ingredients="Water",
+            text="A very long recipe here",
+            period_type_id=1,
+            author_id=9999,
+            slug=f"test-recipe"
+        )
+    db.session.add(recipe)
+    db.session.commit()
+
+    return recipe
+
+
+@pytest.fixture
+def test_set_of_recipes(app):
+    recipes: dict[str, list[Recipe]] = {
+        'visible': [],
+        'hidden': [],
+    }
+    
+    # Visible recipes
+    for num in range(10):
+        recipe = Recipe(
+            name=f"Visible Recipe {num}",
+            calories="4",
+            cooking_time="1337",
+            ingredients="Water",
+            text="A very long recipe here",
+            period_type_id=1,
+            author_id=9999,
+            slug=f"test-slug-visible-{num}"
+        )        
+        db.session.add(recipe)
+        recipes['visible'].append(recipe)
+    
+    # Hidden recipes
+    for num in range(5):
+        recipe = Recipe(
+            name=f"Visible Recipe {num}",
+            calories="0",
+            cooking_time="1337",
+            ingredients="Water",
+            text="A very long recipe here",
+            period_type_id=1,
+            author_id=9999,
+            slug=f"test-slug-hidden-{num}",
+            is_visible=False
+        )          
+        db.session.add(recipe)
+        recipes['hidden'].append(recipe)
+        
+    db.session.commit()
+
+    return recipes
+    
