a38a0de2f64c8c77c2a5af457f06f147b2cc996d
mykdolnyk <mdolnyk@gmail.com>
2026-03-19 20:29:50 +0200

    Made essential tasks run on startup

--- app/make_celery.py
+++ app/make_celery.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 from celery import Celery
-from app_factory import create_app
+from app_factory import create_app, redis_client
 from backend.recipes.tasks import calculate_popular_recipes
 
 flask_app = create_app()
@@ -8,11 +8,29 @@ celery_app: Celery = flask_app.extensions["celery"]
 
 celery_app.autodiscover_tasks(['backend.recipes', 'backend.users'])
 
+LOCK_EXPIRE = 60 # seconds
+
+
 # Periodic Tasks
-@celery_app.on_after_configure.connect
-def run_tasks_on_startup(sender: Celery, **kwargs):
+@celery_app.on_after_finalize.connect
+def add_periodic_tasks(sender: Celery, **kwargs):
     sender.add_periodic_task(
         timedelta(minutes=1),
         # The 1-minute period is for demonstrational purposes only
         calculate_popular_recipes.s(),
         name='calculate popular recipes')
+
+
+# Tasks on Startup
+@celery_app.on_after_finalize.connect
+def run_tasks_on_startup(sender: Celery, **kwargs):
+    task_list = [
+        calculate_popular_recipes
+    ]
+    
+    for task in task_list:
+        lock_id = f'lock:{task.__name__}'
+        lock = redis_client.get(lock_id)
+        if lock is None:
+            task.delay()
+            redis_client.set(lock_id, 1, LOCK_EXPIRE)
